9080026eb12ab04b03a25c03dbc482f20147e45e
Damien-EpiMaillard <damien.maillard@epitech.eu>
2021-10-13 22:07:14 +0200

    [+] request for get information about user

--- lib/home_page/my_profile/my_profile.dart
+++ lib/home_page/my_profile/my_profile.dart
@@ -1,3 +1,4 @@
+import 'package:bsflutter/home_page/my_profile/request/user.dart';
 import 'package:flutter/material.dart';
 
 class MyProfile extends StatefulWidget {
@@ -10,10 +11,39 @@ class MyProfile extends StatefulWidget {
 }
 
 class _MyProfile extends State<MyProfile> {
+  late Future<User> futureUser;
+
+  @override
+  void initState() {
+    super.initState();
+    futureUser = fetchUser();
+  }
 
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Profile d"));
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder<User>(
+          future: futureUser,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data!.avatarUrl == null) {
+                return Text('${snapshot.error}');
+              } else {
+                return CircleAvatar(
+                  backgroundImage: NetworkImage(snapshot.data!.avatarUrl),
+                );
+              }
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
+          },
+        ),
+      ),
+    );
   }
 }
--- lib/home_page/my_profile/request/user.dart
+++ lib/home_page/my_profile/request/user.dart
@@ -1,19 +1,34 @@
 import 'dart:async';
 import 'dart:convert';
-
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart';
+import 'package:localstorage/localstorage.dart';
+
+Future<User> fetchUser() async {
+  final LocalStorage storage = LocalStorage('user');
+  final response = await get(Uri.parse('https://oauth.reddit.com/api/v1/me'), headers: {'authorization': 'Bearer ${storage.getItem('token')}'});
+
+  if (response.statusCode == 200) {
+    return User.fromJson(jsonDecode(response.body));
+  } else {
+    debugPrint('/api/v1/me: ${response.statusCode}');
+    throw Exception('Failed to load user information');
+  }
+}
 
 class User {
   final String displayName;
+  final String avatarUrl;
 
   User({
-    required this.displayName;
-  })
+    required this.displayName,
+    required this.avatarUrl,
+  });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
-      displayName: json['userId'],
+      displayName: json['name'],
+      avatarUrl: json['icon_img'],
     );
   }
 }
